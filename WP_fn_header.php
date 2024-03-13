86611dca82700b7e78e732cedabc64ecd2e7480e
thogroupone <thomas.hoejland@group.one>
2024-03-13 16:35:04 +0100

    Server Container Hostnames for sst.uniweb.no, sst.fastname.no added to replace www.googletagmanager.com hostname

--- WP_fn_header.php
+++ WP_fn_header.php
@@ -9,7 +9,7 @@ $theme_url = trailingslashit( get_stylesheet_directory_uri() );
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://sst.fastname.no/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MB5SPBG');</script>
 <?php endif; ?>
 <!-- End Google Tag Manager -->
@@ -74,7 +74,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <body <?php body_class(); ?>>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MB5SPBG"
+<noscript><iframe src="https://sst.fastname.no/ns.html?id=GTM-MB5SPBG"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <?php wp_body_open(); ?>
